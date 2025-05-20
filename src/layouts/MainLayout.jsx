5e082f8ebf70555efecbbf3b3b598ca8ee85a477
ruhanaatiq <ruhana.atiq@gmail.com>
2025-05-20 10:58:51 +0600

    footer updated

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
   return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
       <main className="min-h-screen">
         <Outlet />
       </main>
+      <Footer></Footer>
     </>
   );
 };
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>ERROR 404</h1>
-        </div>
-    );
+     return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-amber-50 text-center p-6">
+      <img
+        src="https://i.ibb.co/jnVKsRt/error.jpg"
+        alt="404 Food Not Found"
+        className="w-64 mb-6"
+      />
+      <h1 className="text-5xl font-bold text-red-400 mb-4">404</h1>
+      <p className="text-xl font-medium mb-6 text-gray-700">
+        Oops! Looks like the recipe you're looking for has gone missing.
+      </p>
+      <Link
+        to="/"
+        className="btn bg-red-400 hover:bg-red-500 text-white font-semibold px-6 py-2 rounded"
+      >
+        Back to Home
+      </Link>
+    </div>
+  );
 };
-
 export default ErrorPage;
